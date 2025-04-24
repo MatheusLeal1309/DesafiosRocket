306459a664eafdd12a35bc70d306b6d37985a488
matheusleal13 <matheusleal13@hotmail.com>
2025-04-23 22:13:36 -0300

    ajustes

--- prototipos/assets/js/metodos.js
+++ prototipos/assets/js/metodos.js
@@ -8,7 +8,46 @@ Array.prototype.myMap = function (callback) {
     return result;
 };
 
+Array.prototype.myFilter = function (callback) {
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            result.push(this[i]);
+        }
+    }
+    return result;
+};
+
+Array.prototype.myReduce = function (callback, firstValue) {
+    let package = firstValue !== undefined ? firstValue : this[0];
+    let i = firstValue !== undefined ? 0 : 1;
+
+    for (; i < this.length; i++) {
+        package = callback(package, this[i], i, this);
+    }
+    return package;
+};
+
+Array.prototype.myForEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this);
+    }
+};
+
+Array.prototype.myFind = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            return this[i];
+        }
+    }
+    return undefined;
+};
+
 
 const double = numbers.myMap(n => n * 2);
+const pairs = numbers.myFilter(n => n % 2 === 0);
+const sum = numbers.myReduce((a, n) => a + n, 0);
 
 console.log("Map:", double);
+console.log("Filter:", pairs);
+console.log("Reduce:", sum);
