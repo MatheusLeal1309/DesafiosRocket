a975124ea3cc8ae7749e7bfdc26620413e3f5e40
matheusleal13 <matheusleal13@hotmail.com>
2025-04-28 15:09:57 -0300

    adc ajustes

--- gerenciamento-usuario-escola/assets/js/automacao.js
+++ gerenciamento-usuario-escola/assets/js/automacao.js
@@ -1,8 +1,9 @@
 angular.module('escolaApp', [])
     .controller('AppController', function ($scope) {
         $scope.mensagem = "Bem-vindo ao sistema de cadastro escolar";
-        $scope.usuario = {
-            nome: "João",
-            tipo: "Aluno"
-        };
+        $scope.usuarios = [
+            { nome: "João", tipo: "Aluno" },
+            { nome: "Maria", tipo: "Professor" }
+        ];
+
     });
